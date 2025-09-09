83114582b3daed9fe4243a95cfb4df0d8060e70e
csrautomationdep <csrautomationdep@fracistaxservice.com>
2025-09-08 19:07:46 -0600

    Update deployment arguments for public access in cloudbuild.yaml; fix environment variable initialization in agent.py

--- services/agent-sofia/sofia_agent/agent.py
+++ services/agent-sofia/sofia_agent/agent.py
@@ -18,9 +18,9 @@ load_dotenv(dotenv_path=dotenv_path)
 # Use default project from credentials if not in .env
 _, project_id = google.auth.default()
 #Google cloud and Vertex AI configuration
-PROJECT_ID = os.environ.setdefault("GOOGLE_CLOUD_PROJECT")
-LOCATION = os.environ.setdefault("GOOGLE_CLOUD_LOCATION")
-GENAI_USE= os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI")
+PROJECT_ID = os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
+LOCATION = os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "us-central1")
+GENAI_USE= os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
 
 
 logging_client = google_cloud_logging.Client()
